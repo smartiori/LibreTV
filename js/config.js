742e887eadfa35af928daa9031c39036d23f8b25
smartiori <48759613+smartiori@users.noreply.github.com>
2025-08-06 10:48:56 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -128,7 +128,7 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     ckzy: {
-        api: 'https://ckzy.me/api.php/provide/vod/?ac=list',
+        api: 'https://ckzy.me/api.php/provide/vod/at/xml',
         name: 'CK资源',
         adult: true
     },
