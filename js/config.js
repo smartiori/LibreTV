8511b2dd63c48cbc23b9d6a9e15a1912406f2b98
smartiori <48759613+smartiori@users.noreply.github.com>
2026-07-22 08:46:11 +0800

    Disable huangcang configuration in config.js
    Comment out the 'huangcang' configuration to disable it.

--- js/config.js
+++ js/config.js
@@ -161,12 +161,12 @@ const API_SITES = {
         name: 'lsb资源',
         adult: true
     },
-    huangcang: {
-        api: 'https://hsckzy.vip',
-        name: '黄色仓库',
-        adult: true,
-        detail: 'https://hsckzy.vip'
-    },
+    //huangcang: {
+    //    api: 'https://hsckzy.vip',
+    //    name: '黄色仓库',
+    //    adult: true,
+    //    detail: 'https://hsckzy.vip'
+    //},
     yutu: {
         api: 'https://apiyutu.com/api.php/provide/vod/',
         name: '玉兔资源',
