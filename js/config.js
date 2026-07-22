f2065a4d1012dcd7bf58d39280b2fc60551a3323
smartiori <48759613+smartiori@users.noreply.github.com>
2026-07-22 08:42:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -206,7 +206,7 @@ const API_SITES = {
     },    
     huangsec: {
         api: 'https://hsckzy888.com/api.php/provide/vod/',
-        name: '黄色仓库',
+        name: '黄色仓库new',
         adult: true
     },        
     // 下面是资源失效率高的API源，不建议使用
