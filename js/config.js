1dfed2d4c88bea104c466214e098e16704152eba
smartiori <48759613+smartiori@users.noreply.github.com>
2026-07-27 08:35:54 +0800

    Add tianya source configuration

--- js/config.js
+++ js/config.js
@@ -118,6 +118,10 @@ const API_SITES = {
         api: 'https://cj.yayazy.net/api.php/provide/vod/',
         name: '鸭鸭资源站'
     },    
+    tianya: {
+        api: 'https://tyyszyapi.com/api.php/provide/vod/',
+        name: '天涯资源站'
+    },        
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
