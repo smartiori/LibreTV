c455f7ed2da15e4c396dc7946434740250351321
smartiori <48759613+smartiori@users.noreply.github.com>
2026-09-14 08:16:06 +0800

    Add new source configuration for aiqiyi

--- js/config.js
+++ js/config.js
@@ -121,7 +121,11 @@ const API_SITES = {
     tianya: {
         api: 'https://tyyszyapi.com/api.php/provide/vod/',
         name: '天涯资源站'
-    },        
+    },
+    aiqiyi: {
+        api: 'https://iqiyizyapi.com/api.php/provide/vod/',
+        name: '爱奇已2609'
+    },    
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
